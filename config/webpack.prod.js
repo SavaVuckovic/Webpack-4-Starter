71049562e67507f7470fbca1f8f928177db0b264
Sava Vuckovic <vuckovicsava11@gmail.com>
2018-08-09 23:40:41 +0200

    added webpack production configuration

---
+++ config/webpack.prod.js
@@ -0,0 +1,76 @@
+const webpack = require('webpack');
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: ['./src/js/index.js']
+  },
+  mode: 'production',
+  output: {
+    filename: '[name]-bundle.js',
+    path: path.resolve(__dirname, '../dist'),
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader'
+          }
+        ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCSSExtractPlugin.loader
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'postcss-loader'
+          },
+          {
+            loader: "sass-loader"
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              attrs: ['img:src']
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(jpg|jpeg|gif|png)/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'images/[name]-[hash:8].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new OptimizeCSSAssetsPlugin(),
+    new MiniCSSExtractPlugin(),
+    new HTMLWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
+};
